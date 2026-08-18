db96509862801b5dd5e130ac669a80a90fb4bf7d
Kevin Esteban Rincon Moreno <kevin_5877@hotmail.com>
2026-08-17 21:16:56 -0500

    fix: Corregida importación del modelo Base de SQLAlchemy

--- api/alembic/env.py
+++ api/alembic/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 from infrastructure.configuracion import configuracion
-from infrastructure.models import Base
+from infrastructure.models.base import Base
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
