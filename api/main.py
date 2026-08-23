79dabd4f124179c48b0885937f3451624039719c
Kevin Esteban Rincon Moreno <kevin_5877@hotmail.com>
2026-08-23 09:05:28 -0500

    inicializado firebase app check y agregado middleware que se encarga de la comprobación del token en la app

--- api/main.py
+++ api/main.py
@@ -1,14 +1,23 @@
 from fastapi import FastAPI
 
 from autenticacion import enrutador as login
+from infrastructure.configuracion import configuracion
 
 from application.enrutador_buses import enrutador as enrutador_buses
 from application.enrutador_rastreadores import enrutador as enrutador_rastreadores
 from application.enrutador_rutas import enrutador as enrutador_rutas
 from application.enrutador_usuarios import enrutador as enrutador_usuarios
 from application.enrutador_lugares import enrutador as enrutador_lugares
+import firebase_admin
+from firebase_admin import credentials
 
 aplicacion = FastAPI()
+
+from application.middleware.firebase_app_check import comprobar_token_firebase
+
+
+cred = credentials.Certificate(configuracion.firebase_credentials_path)
+firebase_admin.initialize_app(cred)
 
 aplicacion.include_router(login)
 aplicacion.include_router(enrutador_buses)
