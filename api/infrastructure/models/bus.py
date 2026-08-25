651ca4ec2ff03f035b0576613b381b1d26680f0f
Kevin Esteban Rincon Moreno <kevin_5877@hotmail.com>
2026-08-25 15:43:37 -0500

    refactor: eliminados todos los modelos relacionados con los rastreadores para ser movidos a otro archivo

--- api/infrastructure/models/bus.py
+++ api/infrastructure/models/bus.py
@@ -20,31 +20,4 @@ class Empresa(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     nombre: Mapped[str] = mapped_column(String(100))
 
-class Rastreador(Base):
-    __tablename__ = "rastreadoresadmin_rastreador"
-    serial: Mapped[str] = mapped_column(String(50),primary_key=True)
-    modelo: Mapped[str] = mapped_column(String(100))
-    imei: Mapped[str] = mapped_column(String(15))
-    iccid: Mapped[str] = mapped_column(String(22))
-    operador_red_id: Mapped[int] = mapped_column(Integer,ForeignKey("rastreadoresadmin_operadorredmovil.id"))
-    operador_red: Mapped["OperadorRedMovil"] = relationship()
-    numero_sim: Mapped[str] = mapped_column(String(15))
-    empresa_id: Mapped[int] = mapped_column(Integer,ForeignKey('loginuser_empresa.id'))
-    empresa: Mapped["Empresa"] = relationship()
-    asignaciones: Mapped[List["AsignacionRastreador"]] = relationship(back_populates="rastreador")
 
-class OperadorRedMovil(Base):
-    __tablename__ = "rastreadoresadmin_operadorredmovil"
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    nombre: Mapped[str] = mapped_column(String(20))
-
-class AsignacionRastreador(Base):
-    __tablename__ = "busadmin_asignacionrastreadores"
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    fecha_inicio: Mapped[timezone] = mapped_column(DateTime(timezone=True))
-    fecha_fin: Mapped[timezone | None] = mapped_column(DateTime(timezone=True))
-    bus_id: Mapped[str] = mapped_column(ForeignKey("busadmin_bus.placa"))
-    rastreador_id: Mapped[str] = mapped_column(ForeignKey("rastreadoresadmin_rastreador.serial"))
-    rastreador: Mapped["Rastreador"] = relationship(
-        back_populates="asignaciones"
-    )
