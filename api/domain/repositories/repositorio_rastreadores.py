bc16ada90a32d56b926a4efc947c2868c4fdf8d8
Kevin Esteban Rincon Moreno <kevin_5877@hotmail.com>
2026-08-25 09:49:52 -0500

    add: agregados campos ruta_horario_inicio y ruta_horario_fin en el modelo del dominio ''

--- api/domain/repositories/repositorio_rastreadores.py
+++ api/domain/repositories/repositorio_rastreadores.py
@@ -3,12 +3,14 @@ from abc import ABC, abstractmethod
 from pydantic import BaseModel
 
 from domain.entities.models import Coordenada
-
+from datetime import time
 
 class EstadoOperativoRastreador(BaseModel):
     numero_bus: int
     empresa_id: int
     ruta_codigo: str
+    ruta_hora_inicio: time
+    ruta_hora_fin: time
     paradero_inicio: Coordenada
     paradero_final: Coordenada
 
