b6b1575b8c837142df1f41bff6a3d831f290e741
Kevin Esteban Rincon Moreno <kevin_5877@hotmail.com>
2026-08-25 15:42:43 -0500

    add: agregados nuevos métodos al repositorio, obtener_rastreador_por_serial, verificar_rastreador_registrado entre otros. esto para cumplir con el requerimiento de la autenticación de los rastreadores

--- api/domain/repositories/repositorio_rastreadores.py
+++ api/domain/repositories/repositorio_rastreadores.py
@@ -1,8 +1,10 @@
 from abc import ABC, abstractmethod
+from inspect import walktree
 
 from pydantic import BaseModel
 
 from domain.entities.models import Coordenada
+from domain.entities.rastreador import Rastreador
 from datetime import time
 
 class EstadoOperativoRastreador(BaseModel):
@@ -17,4 +19,16 @@ class EstadoOperativoRastreador(BaseModel):
 class RepositorioRastreadores(ABC):
     @abstractmethod
     def obtener_estado_operativo(self, serial: str) -> EstadoOperativoRastreador | None:
+        pass
+    @abstractmethod
+    def obtener_rastreador_por_serial(self,serial:str) -> Rastreador | None:
+        pass
+    @abstractmethod
+    def verificar_rastreador_registrado(self,serial:str) -> bool:
+        pass
+    @abstractmethod
+    def registrar_secreto(self,serial:str,secreto_hasheado:str) -> None:
+        pass
+    @abstractmethod
+    def obtener_hash_secreto(self,serial:str) -> str | None:
         pass
