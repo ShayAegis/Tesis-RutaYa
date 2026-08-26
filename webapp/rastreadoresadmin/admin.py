b0b66adf61055de2a9540305a01db8a2c393ec0b
Kevin Esteban Rincon Moreno <kevin_5877@hotmail.com>
2026-08-26 11:11:56 -0500

    registrado modelo Rastreadores para que pueda ser accedido y modificado desde el admin panel de Django

--- webapp/rastreadoresadmin/admin.py
+++ webapp/rastreadoresadmin/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from .models import OperadorRedMovil
+from .models import OperadorRedMovil, Rastreador
 
 # Register your models here.
 
 admin.site.register(OperadorRedMovil)
+
+
+@admin.register(Rastreador)
+class RastreadorAdmin(admin.ModelAdmin):
+    list_display = ("serial", "modelo", "imei", "iccid", "numero_sim", "operador_red", "empresa")
+    search_fields = ("serial", "modelo", "imei", "iccid", "numero_sim")
+    list_filter = ("operador_red", "empresa")
 
