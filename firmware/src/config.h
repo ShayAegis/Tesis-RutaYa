97cd7c2b392e9fd47ff29d49b9aa3dab68405370
Kevin Esteban Rincon Moreno <kevin_5877@hotmail.com>
2026-08-30 12:15:16 -0500

    actualizadas configuraciones del firmware, editando la nueva URL de la API, el host MQTT, agregados campos para el usuario y contraseña MQTT, agregado el endpoint de aprovisionamiento y otras configuraciones relacionadas con NVS

--- firmware/src/config.h
+++ firmware/src/config.h
@@ -7,10 +7,14 @@ constexpr const char* serialTracker = "RY-TR-2026-0001";
 
 constexpr const char* apn = "internet.tigo.co.com";
 
-constexpr const char* mqttBrokerUrl =  "broker.hivemq.com";
-constexpr const char* mqttTopicBase = "testsim7000/";
+constexpr const char* mqttBrokerUrl =  "tracking.tesis.rutaya.xyz";
+constexpr const char* mqttTopicBase = "buses/";
 constexpr int mqttPort = 1883;
 constexpr int mqttQos = 1;
 
-constexpr const char* busApiBaseUrl = "https://pulled-tech-counted-shared.trycloudflare.com";
+constexpr const char* busApiBaseUrl = "https://api.tesis.rutaya.xyz";
 constexpr const char* busApiEndpoint = "/rastreadores/me";
+constexpr const char* provisionEndpoint = "/rastreadores/aprovisionar";
+
+constexpr const char* nvsNamespace = "rutaya";
+constexpr const char* nvsSecretKey = "secreto";
