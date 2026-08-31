af7252db8eaed0a6a97540b8378707691f787f49
Kevin Esteban Rincon Moreno <kevin_5877@hotmail.com>
2026-08-31 12:33:06 -0500

    Inyectado el `NvsStorage` al repositorio que obtiene la información del bus

--- firmware/src/infrastructure/repository/GetBusInformationBySim.h
+++ firmware/src/infrastructure/repository/GetBusInformationBySim.h
@@ -1,13 +1,15 @@
 #pragma once
 #include "domain/BusInformationRepository.h"
 #include "infrastructure/Sim7000.h"
+#include "infrastructure/NvsStorage.h"
 
 class BusInformationRepositoryBySim : public IBusInformationRepository {
     private:
         Sim7000G& _sim;
+        NvsStorage& _nvs;
         const char* _baseUrl;
         const char* _endpoint;
     public:
         TrackerOperatingState getTrackerState() override;
-        BusInformationRepositoryBySim(Sim7000G& sim, const char* url, const char* endpoint) : _sim(sim), _baseUrl(url), _endpoint(endpoint) {}
+        BusInformationRepositoryBySim(Sim7000G& sim, NvsStorage& nvs, const char* url, const char* endpoint) : _sim(sim), _nvs(nvs), _baseUrl(url), _endpoint(endpoint) {}
 };
